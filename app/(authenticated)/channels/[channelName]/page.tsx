3caac438d7afce7598044bfb5659948c6196f004
Mikael Weill <weillmikael@gmail.com>
2025-01-09 22:12:43 -0800

    put the message input at the bottom of the screen

--- app/(authenticated)/channels/[channelName]/page.tsx
+++ app/(authenticated)/channels/[channelName]/page.tsx
@@ -14,14 +14,18 @@ export default async function ChannelPage({ params, searchParams }: ChannelPageP
   const { user, currentChannel, initialMessages } = await getChannelData(channelName)
 
   return (
-    <div className="flex-1 flex flex-col">
-      <MessageList 
-        initialMessages={initialMessages} 
-        channelId={currentChannel.id}
-        currentUserId={user.id}
-        messageIdToScrollTo={messageId}
-      />
-      <MessageInput channelId={currentChannel.id} />
+    <div className="flex-1 flex flex-col h-[calc(100vh-4rem)]">
+      <div className="flex-1 overflow-hidden">
+        <MessageList 
+          initialMessages={initialMessages} 
+          channelId={currentChannel.id}
+          currentUserId={user.id}
+          messageIdToScrollTo={messageId}
+        />
+      </div>
+      <div className="sticky bottom-0 bg-white border-t">
+        <MessageInput channelId={currentChannel.id} />
+      </div>
     </div>
   )
 } 
